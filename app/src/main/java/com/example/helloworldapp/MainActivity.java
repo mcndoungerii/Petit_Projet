bad37f0c4689ccc6fe27611a187860b7086f7e4a
mcndoungerii <allenmcndounger@gmail.com>
2021-01-26 17:37:00 +0300

    first commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,4 @@
+package com.example.helloworldapp;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/example/helloworldapp/Petit.java
@@ -0,0 +1,10 @@
+package com.example.helloworldapp;
+
+public class Petit {
+    public static void main(String[] args) {
+        String lastName = "Ndunga";
+        int age = 31;
+
+        System.out.println("Hi, My name is " + lastName + ".I am " + age + "years old");
+    }
+}
